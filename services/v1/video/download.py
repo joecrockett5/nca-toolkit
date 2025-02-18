15262696cbddbe703d60f2ce01bc64329b2bdc62
Joe Crockett <joe@crockett.net>
2025-02-18 19:56:18 +0000

    Further constrain video dimesions to 1920x1080
    Guarentee .mp4 is the merged video format to prevent issues downstream.

--- services/v1/video/download.py
+++ services/v1/video/download.py
@@ -15,7 +15,8 @@ COOKIE_FILE_PATH = "/tmp/cookiefile.txt"
 def download_yt_video(yt_url: str, cookiefile_path: str, job_id: str) -> dict[str, str]:
     """Download a video from a URL."""
     ydl_opts = {
-        "format": "bestvideo[height<=1080]+bestaudio/best[height<=1080]",
+        "format": "bestvideo[width<=1920][height<=1080]+bestaudio/best[width<=1920][height<=1080]",
+        "merge_output_format": "mp4",
         "outtmpl": f"{STORAGE_PATH}{job_id}.%(ext)s",
         "restrictfilenames": True,
         "cookiefile": cookiefile_path,
